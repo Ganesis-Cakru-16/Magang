ed00e6f036b9b9a278295f954aa9afb40621d6b4
varelll <19623183@std.stei.itb.ac.id>
2024-05-11 16:01:04 +0700

    white

--- Source Code/hsvtowhite.py
+++ Source Code/hsvtowhite.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
 
-def white():
-    # Open the video file
+def extract_white():
+    # Buka file video
     cap = cv2.VideoCapture(0)
 
     while cap.isOpened():
@@ -10,18 +10,25 @@ def white():
         if not ret:
             break
 
-        # Convert BGR to HSV
+        # Konversi dari BGR ke HSV
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-        # Define range of white color in HSV
+        # Tentukan range warna putih dalam HSV
         lower_white = np.array([0, 0, 200])
         upper_white = np.array([180, 25, 255])
 
-        # Threshold the HSV image to get only white colors
-        mask = cv2.inRange(hsv, lower_white, upper_white)
+        # Buat mask untuk warna putih
+        mask_white = cv2.inRange(hsv, lower_white, upper_white)
 
-        # Bitwise-AND mask and original image
-        res = cv2.bitwise_and(frame, frame, mask=mask)
+        # Hilangkan noise dengan operasi morfologi
+        kernel = np.ones((5, 5), np.uint8)
+        mask_white_open = cv2.morphologyEx(mask_white, cv2.MORPH_OPEN, kernel)
+
+        # Gabungkan beberapa mask untuk menghilangkan bayangan
+        mask_final = cv2.bitwise_and(mask_white, mask_white_open)
+
+        # Aplikasikan mask ke frame asli
+        res = cv2.bitwise_and(frame, frame, mask=mask_final)
 
         cv2.imshow('Original', frame)
         cv2.imshow('Only White', res)
@@ -32,5 +39,5 @@ def white():
     cap.release()
     cv2.destroyAllWindows()
 
-# Example usage
-white()
+# Contoh penggunaan
+extract_white()
