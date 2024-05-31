cf3c7618ee0fadbd027d5c6ca181e7e7d839a273
MonetaryApe450 <167334435+MonetaryApe450@users.noreply.github.com>
2024-06-01 02:05:32 +0700

    feat: comvis, aruco marker distance estimation
    ayamgoreng terisi matriks kalibrasi camera logitech
    CameraCal terisi program untuk menghitung kalibrasi kamera
    Distace.py program untuk menghitung distance

---
+++ Source Code/comvis/DISTANCE-ESTIMATION/distance.py
@@ -0,0 +1,104 @@
+import cv2 as cv
+from cv2 import aruco
+import numpy as np
+
+calib_data_path = "./ayamgoreng/MultiMatrix.npz"
+
+calib_data = np.load(calib_data_path)
+print(calib_data.files)
+
+cam_mat = calib_data["camMatrix"]
+dist_coef = calib_data["distCoef"]
+r_vectors = calib_data["rVector"]
+t_vectors = calib_data["tVector"]
+
+MARKER_SIZE = 3 # centimeters
+ARUCO_DICT = {
+	"DICT_4X4_50": cv.aruco.DICT_4X4_50,
+	"DICT_4X4_100": cv.aruco.DICT_4X4_100,
+	"DICT_4X4_250": cv.aruco.DICT_4X4_250,
+	"DICT_4X4_1000": cv.aruco.DICT_4X4_1000,
+	"DICT_5X5_50": cv.aruco.DICT_5X5_50,
+	"DICT_5X5_100": cv.aruco.DICT_5X5_100,
+	"DICT_5X5_250": cv.aruco.DICT_5X5_250,
+	"DICT_5X5_1000": cv.aruco.DICT_5X5_1000,
+	"DICT_6X6_50": cv.aruco.DICT_6X6_50,
+	"DICT_6X6_100": cv.aruco.DICT_6X6_100,
+	"DICT_6X6_250": cv.aruco.DICT_6X6_250,
+	"DICT_6X6_1000": cv.aruco.DICT_6X6_1000,
+	"DICT_7X7_50": cv.aruco.DICT_7X7_50,
+	"DICT_7X7_100": cv.aruco.DICT_7X7_100,
+	"DICT_7X7_250": cv.aruco.DICT_7X7_250,
+	"DICT_7X7_1000": cv.aruco.DICT_7X7_1000,
+	"DICT_ARUCO_ORIGINAL": cv.aruco.DICT_ARUCO_ORIGINAL,
+	"DICT_APRILTAG_16h5": cv.aruco.DICT_APRILTAG_16h5,
+	"DICT_APRILTAG_25h9": cv.aruco.DICT_APRILTAG_25h9,
+	"DICT_APRILTAG_36h10": cv.aruco.DICT_APRILTAG_36h10,
+	"DICT_APRILTAG_36h11": cv.aruco.DICT_APRILTAG_36h11
+}
+marker_dict = aruco.Dictionary_get(aruco.DICT_5X5_1000)
+
+param_markers = aruco.DetectorParameters_create()
+
+cap = cv.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    marker_corners, marker_IDs, reject = aruco.detectMarkers(
+        gray_frame, marker_dict, parameters=param_markers
+    )
+    if marker_corners:
+        rVec, tVec, _ = aruco.estimatePoseSingleMarkers(
+            marker_corners, MARKER_SIZE, cam_mat, dist_coef
+        )
+        total_markers = range(0, marker_IDs.size)
+        for ids, corners, i in zip(marker_IDs, marker_corners, total_markers):
+            cv.polylines(
+                frame, [corners.astype(np.int32)], True, (0, 255, 255), 1, cv.LINE_AA
+            )
+            corners = corners.reshape(4, 2)
+            corners = corners.astype(int)
+            top_right = corners[0].ravel()
+            top_left = corners[1].ravel()
+            bottom_right = corners[2].ravel()
+            bottom_left = corners[3].ravel()
+
+            # Since there was mistake in calculating the distance approach point-outed in the Video Tutorial's comment
+            # so I have rectified that mistake, I have test that out it increase the accuracy overall.
+            # Calculating the distance
+            distance = np.sqrt(
+                tVec[i][0][2] ** 2 + tVec[i][0][0] ** 2 + tVec[i][0][1] ** 2
+            )
+            # Draw the pose of the marker
+            point = cv.drawFrameAxes(frame, cam_mat, dist_coef, rVec[i], tVec[i], 4, 1)
+            cv.putText(
+                frame,
+                f"id: {ids[0]} Dist: {round(distance, 2)}",
+                top_right,
+                cv.FONT_HERSHEY_PLAIN,
+                1.3,
+                (0, 0, 255),
+                1,
+                cv.LINE_AA,
+            )
+            cv.putText(
+                frame,
+                f"x:{round(tVec[i][0][0],1)} y: {round(tVec[i][0][1],1)} ",
+                bottom_right,
+                cv.FONT_HERSHEY_PLAIN,
+                1.0,
+                (0, 0, 255),
+                1,
+                cv.LINE_AA,
+            )
+            # print(ids, "  ", corners)
+    cv.imshow("frame", frame)
+    cv.imshow("brame", gray_frame)
+    key = cv.waitKey(1)
+    if key == ord("q"):
+        break
+cap.release()
+cv.destroyAllWindows()
